fd25d55ace9f1c5be7995af25342b1c2e549d514
GonaloAlves <alvesgoncas2014@gmail.com>
2024-09-23 16:06:41 +0000

    finish doc string and comments on immune analysis

--- src/new_immune.py
+++ src/new_immune.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # Step 1: Load the dataset
 def load_data(file_path):
     """
-    Load the single-cell data from a .h5ad file.
+    Load the data from a .h5ad file.
     
     Parameters:
     file_path (str): Path to the .h5ad file.
@@ -22,7 +22,7 @@ def load_data(file_path):
 # Step 2: Extract Differentially Expressed Genes (DEGs)
 def extract_dge_data(adata):
     """
-    Extract the differential gene expression data from the AnnData object.
+    Extract the different type of data from the AnnData object like the differential gene expression values from each cell.
 
     Parameters:
     adata (AnnData): The AnnData object containing the DGE data.
@@ -45,27 +45,31 @@ def extract_dge_data(adata):
 # Step 3: Create cluster dataframes with filtered data
 def create_cluster_dfs(gene_names, logfoldchanges, pvals_adj, scores, pts, sort_by_logfc=False, pts_threshold=0):
     """
-    Create a dictionary of dataframes per cluster, with filtered and optionally sorted genes.
+    Create a dictionary of dataframes per cluster, with the respective gene expression data.
+    By default this function will not order the genes by fold change and the default minimun pts is 0
 
     Parameters:
     gene_names (DataFrame): DataFrame with gene names.
-    logfoldchanges (DataFrame): DataFrame with log fold changes for each gene.
-    pvals_adj (DataFrame): DataFrame with adjusted p-values for each gene.
-    scores (DataFrame): DataFrame with scores for each gene.
-    pts (DataFrame): DataFrame with "pts" values for each gene.
+    logfoldchanges (DataFrame): DataFrame with logfoldchange values for each gene from each cell.
+    pvals_adj (DataFrame): DataFrame with adjusted p-values for each gene from each cell.
+    scores (DataFrame): DataFrame with willcoxon scores for each gene from each cell.
+    pts (DataFrame): DataFrame with p values for each gene from each cell.
     sort_by_logfc (bool): If True, sort the filtered DataFrame by log fold change in descending order.
-    pts_threshold (float): The minimum 'pts' value to filter genes.
+    pts_threshold (float): The minimum percentage of expression value to filter genes.
+
+    logFoldChange: Magnitude of the difference in gene expression between the compared groups.
+    Pts: Proportion of cells in each group that express a particular gene
 
     Returns:
-    dict: Dictionary of filtered DataFrames for each cluster.
+    dict: Dictionary of DataFrames for each cluster.
     """
-    cluster_dfs = {}
+    cluster_dfs = {} # creates dictionary
 
-    for i in range(len(gene_names.columns)):  # For each cluster
-        gene_reindex = gene_names.iloc[:, i]  # Reindex genes for pts
-        pts_reindexed = pts.iloc[:, i].reindex(gene_reindex.values)  # Reindex the pts
+    for i in range(len(gene_names.columns)):  # For to read each cluster
+        gene_reindex = gene_names.iloc[:, i]  # Takes correct order of the genes index of the gene names
+        pts_reindexed = pts.iloc[:, i].reindex(gene_reindex.values)  # Reindex the pts with the gene names index
 
-        # Create dataframe with relevant data
+        # Create dataframe for each cluster
         cluster_df = pd.DataFrame({
             'score': scores.iloc[:, i].values,
             'logfoldchange': logfoldchanges.iloc[:, i].values,
@@ -78,12 +82,13 @@ def create_cluster_dfs(gene_names, logfoldchanges, pvals_adj, scores, pts, sort_
         mask_pts = (cluster_df['pts'] >= pts_threshold)
         filtered_df = cluster_df[mask_pts]
 
-        # Optionally sort by log fold change if sort_by_logfc is True
+        # Sort by log fold change if sort_by_logfc is True
         if sort_by_logfc:
             filtered_df = filtered_df.sort_values(by='logfoldchange', ascending=False)
 
+        # Assigns the cluster name as the name of the resolution atributed
         cluster_names = gene_names.columns[i]
-        cluster_dfs[cluster_names] = filtered_df  # Store in dictionary
+        cluster_dfs[cluster_names] = filtered_df  # Store the respective clusters in the dictionary
 
     return cluster_dfs
 
@@ -91,18 +96,18 @@ def create_cluster_dfs(gene_names, logfoldchanges, pvals_adj, scores, pts, sort_
 # Step 4: Remove any cluster
 def remove_clusters_by_suffix(cluster_dfs, suffix):
     """
-    Remove clusters whose names end with a specific suffix from the cluster_dfs dictionary.
+    Remove clusters whose names end with a specific suffix corresponding to the resolution number, or if the case is the NA cluster, all are integrated in the dictionary with every cluster.
 
     Parameters:
     cluster_dfs (dict): Dictionary of clusters and their data.
-    suffix (str): The suffix that determines which clusters to remove (e.g., 'NA').
+    suffix (str): The suffix that determines which clusters to remove (e.g., 'NA', '8.0.2').
 
     Returns:
     dict: Updated dictionary with specified clusters removed.
     """
     clusters_to_delete = [cluster for cluster in cluster_dfs if cluster.endswith(suffix)]
 
-    for cluster in clusters_to_delete:
+    for cluster in clusters_to_delete: # Searches the specific cluster in the dic
         del cluster_dfs[cluster]
 
     return cluster_dfs
@@ -112,13 +117,14 @@ def remove_clusters_by_suffix(cluster_dfs, suffix):
 
 def filter_pval(df):
     """
-    Filter genes by p-value and select the top 5 genes.
+    First step of 6 in order to select the top genes:
+    Filter genes by adj p-value and select the top 5 statisticaly significant genes.
 
     Parameters:
     df (DataFrame): DataFrame containing genes for a cluster.
 
     Returns:
-    DataFrame: Filtered DataFrame with top 5 genes.
+    DataFrame: Filtered DataFrame with top 5 statisticaly significant genes .
     """
     mask_pval = df['pvals_adj'] < 0.05
     filtered_df = df[mask_pval]
@@ -127,7 +133,7 @@ def filter_pval(df):
 
 def has_neg_lfc(top_genes):
     """
-    Check if the top genes contain any negative log fold changes.
+    Step 2 of 6 to determinate if the cluster have any negative log fold change genes (under expressed).
 
     Parameters:
     top_genes (DataFrame): DataFrame of the top 5 genes for a cluster.
@@ -140,7 +146,7 @@ def has_neg_lfc(top_genes):
 
 def count_neg_lfc(top_genes):
     """
-    Count the number of genes with negative log fold changes.
+    Step 3 of 6 to check how many of negative log fold change genes exist (under expressed).
 
     Parameters:
     top_genes (DataFrame): DataFrame of the top 5 genes for a cluster.
@@ -153,7 +159,7 @@ def count_neg_lfc(top_genes):
 
 def replace_neg_lfc(df, top_genes, num_nega_lfc):
     """
-    Replace genes with negative log fold change with new genes.
+    Step 4 of 6 that replaces the negative log fold change genes with the positive ones from the unfiltered df
 
     Parameters:
     df (DataFrame): Original DataFrame of the cluster.
@@ -161,25 +167,25 @@ def replace_neg_lfc(df, top_genes, num_nega_lfc):
     num_nega_lfc (int): Number of genes with negative log fold changes.
 
     Returns:
-    DataFrame: DataFrame with negative log fold change genes replaced.
+    DataFrame: DataFrame with the top 5 genes.
     """
-    pos_lfc = df.head(num_nega_lfc)  # Replace with genes from the original df
+    # Replace the selected genes with the genes from the original df
     top_genes_positive = top_genes[top_genes['logfoldchange'] > 0]
 
     replacements = []
     for gene, values in df.iterrows():
         if gene not in top_genes_positive.index:
-            replacements.append(values)
+            replacements.append(values) # Prevent of appending duplicated genes
         if len(replacements) == num_nega_lfc:
             break  # Stop once enough replacement genes are found
 
     replacements_df = pd.DataFrame(replacements)
-    return pd.concat([top_genes_positive, replacements_df])
+    return pd.concat([top_genes_positive, replacements_df]) # concact the 2 lists
 
 
 def replace_all(df):
     """
-    Select the top 5 genes as a fallback if no filtered genes are found.
+    Step 5 of 6 where the program selects the top 5 genes as a fallback if no filtered genes are found.
 
     Parameters:
     df (DataFrame): Original DataFrame of the cluster.
@@ -192,6 +198,7 @@ def replace_all(df):
 
 def select_top_genes(cluster_dfs):
     """
+    Final step of the selection of apropriated genes 6 of 6:
     Main function to select the top 5 genes per cluster, replacing genes with negative log fold change.
 
     Parameters:
@@ -203,15 +210,15 @@ def select_top_genes(cluster_dfs):
     top_genes_cluster = {}
     
     for cluster, df in cluster_dfs.items():
-        top_genes = filter_pval(df)
+        top_genes = filter_pval(df) # First remove the not significant genes 
 
-        if has_neg_lfc(top_genes):
+        if has_neg_lfc(top_genes): # If there is any neg lfc genes
             num_nega_lfc = count_neg_lfc(top_genes)
-            top_genes_cluster[cluster] = replace_neg_lfc(df, top_genes, num_nega_lfc)
+            top_genes_cluster[cluster] = replace_neg_lfc(df, top_genes, num_nega_lfc) # replace the neg genes
         elif top_genes.empty:
-            top_genes_cluster[cluster] = replace_all(df)
+            top_genes_cluster[cluster] = replace_all(df) # replace all if it is empty
         else:
-            top_genes_cluster[cluster] = top_genes
+            top_genes_cluster[cluster] = top_genes # if the cluster doesnt have neg genes just continues
 
     return top_genes_cluster
 
@@ -296,6 +303,3 @@ if __name__ == "__main__":
     
     # Create dotplot of the top genes
     create_dotplot(adata, top_genes_names)
-
-
-#test
