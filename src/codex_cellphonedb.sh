fc651e4255a98b95582f5e75a0389b1416ef3969
GonaloAlves <alvesgoncas2014@gmail.com>
2025-06-20 11:30:28 +0100

    COMMIT 200 WOOOOOOOOOOOOOOWWWWWWW TRABALHO BUEEEE LESS GOO 2735 WAZAAAAA

--- src/codex_cellphonedb.sh
+++ src/codex_cellphonedb.sh
@@ -22,8 +22,8 @@ conda activate mkpy
 
 # python /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/sigs/common_interactions_sigs.py
 
-Rscript /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/sigs/r_chord_sigs.r
+#Rscript /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/sigs/r_chord_sigs.r
 
-# python /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/sigs/percentages.py
+python /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/sigs/percentages.py
 
 # python /home/makowlg/Documents/Immune-CCI/src/scripts/scripts_cellphonedb/summary_dani.py
